bab349f93dc382375e37e1ef536d1c7d016cf731
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-16 19:18:41 +0300

    Initial commit

---
+++ src/main/java/by/aleksandrphilimonov/testingproject/JwtCore.java
@@ -0,0 +1,7 @@
+package by.aleksandrphilimonov.testingproject;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtCore {
+}
---
+++ src/test/java/by/aleksandrphilimonov/testingproject/TestingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package by.aleksandrphilimonov.testingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
