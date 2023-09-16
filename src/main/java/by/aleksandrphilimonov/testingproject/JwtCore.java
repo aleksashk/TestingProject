51043b72d80dfdef008eb7f1b3f0917acd20d3ab
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-16 19:23:56 +0300

    add @Value to JwtCore class

--- src/main/java/by/aleksandrphilimonov/testingproject/JwtCore.java
+++ src/main/java/by/aleksandrphilimonov/testingproject/JwtCore.java
@@ -1,7 +1,13 @@
 package by.aleksandrphilimonov.testingproject;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtCore {
+    @Value("${testing.app.secret}")
+    private String secret;
+
+    @Value("${testing.app.expiretion}")
+    private int lifetime;
 }
