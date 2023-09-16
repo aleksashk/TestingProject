17eda35c108ac654b86a476dff421c35d11acb07
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-16 19:54:38 +0300

    add class User, UserDetailsImpl, JwtCore

--- src/main/java/by/aleksandrphilimonov/testingproject/JwtCore.java
+++ src/main/java/by/aleksandrphilimonov/testingproject/JwtCore.java
@@ -1,7 +1,12 @@
 package by.aleksandrphilimonov.testingproject;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 public class JwtCore {
@@ -10,4 +15,12 @@ public class JwtCore {
 
     @Value("${testing.app.expiretion}")
     private int lifetime;
+
+    public String generateToken(Authentication authentication) {
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        return Jwts.builder().setSubject((userDetails.getUsername())).setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + lifetime))
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
 }
---
+++ src/main/java/by/aleksandrphilimonov/testingproject/User.java
@@ -0,0 +1,22 @@
+package by.aleksandrphilimonov.testingproject;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+}
